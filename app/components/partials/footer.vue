af40cbc4ec190f09f5da6077d67b00e16b647950
Jaime Garcia Garcia <jgarcia16432@gmail.com>
2023-04-25 22:58:12 -0700

    feat: add interactive social media icons to site

--- app/components/partials/footer.vue
+++ app/components/partials/footer.vue
@@ -1,63 +1,43 @@
 <template>
   <footer class="footer">
     <div class="container">
-      <!-- <div>
-        <h2>Contact</h2>
-        <p v-if="isSignedUp">Thank you - we'll be in touch shortly.</p>
-        <form
-          v-else
-          @submit.prevent="handleSubmit"
-          name="signups"
-          netlify
-          class="flex items-center border-b border-b-2 border-blue-400 py-2"
-        >
-          <input
-            ref="emailInput"
-            v-model="form.email"
-            type="text"
-            name="email"
-            placeholder="your@email.com"
-            aria-label="Email address"
-          />
-          <button type="submit">Submit</button>
-        </form>
-      </div> -->
       <div class="social">
-        <h2>Social</h2>
-        <a target="_blank" href="https://www.linkedin.com/in/jaime-gg/">
-          <svg
-            version="1.1"
-            id="Layer_1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            x="0px"
-            y="0px"
-            viewBox="0 0 448 512"
-            xml:space="preserve"
-          >
-            <path
-              class="st0"
-              d="M100.3,448H7.4V148.9h92.9V448z M53.8,108.1C24.1,108.1,0,83.5,0,53.8C0,24.1,24.1,0,53.8,0
+        <h2>Socials</h2>
+        <div class="social-wrapper">
+          <a target="_blank" title="LinkedIn" href="https://www.linkedin.com/in/jaime-gg/">
+            <svg
+              version="1.1"
+              id="Layer_1"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              x="0px"
+              y="0px"
+              viewBox="0 0 448 512"
+              xml:space="preserve"
+            >
+              <path
+                class="st0"
+                d="M100.3,448H7.4V148.9h92.9V448z M53.8,108.1C24.1,108.1,0,83.5,0,53.8C0,24.1,24.1,0,53.8,0
 	c29.7,0,53.8,24.1,53.8,53.8C107.6,83.5,83.5,108.1,53.8,108.1z M447.9,448h-92.7V302.4c0-34.7-0.7-79.2-48.3-79.2
 	c-48.3,0-55.7,37.7-55.7,76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5,42.7-48.3,87.9-48.3c94,0,111.3,61.9,111.3,142.3V448H447.9z
 	"
-            />
-          </svg>
-        </a>
-        <a target="_blank" href="https://github.com/JaimeGG64">
-          <svg
-            version="1.1"
-            id="Layer_1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            x="0px"
-            y="0px"
-            viewBox="0 0 496 512"
-            xml:space="preserve"
-          >
-            <path
-              class="st0"
-              d="M165.9,397.4c0,2-2.3,3.6-5.2,3.6c-3.3,0.3-5.6-1.3-5.6-3.6c0-2,2.3-3.6,5.2-3.6
+              />
+            </svg>
+          </a>
+          <a target="_blank" title="GitHub" href="https://github.com/JaimeGG64">
+            <svg
+              version="1.1"
+              id="Layer_1"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              x="0px"
+              y="0px"
+              viewBox="0 0 496 512"
+              xml:space="preserve"
+            >
+              <path
+                class="st0"
+                d="M165.9,397.4c0,2-2.3,3.6-5.2,3.6c-3.3,0.3-5.6-1.3-5.6-3.6c0-2,2.3-3.6,5.2-3.6
 	C163.3,393.5,165.9,395.1,165.9,397.4z M134.8,392.9c-0.7,2,1.3,4.3,4.3,4.9c2.6,1,5.6,0,6.2-2s-1.3-4.3-4.3-5.2
 	C138.4,389.9,135.5,390.9,134.8,392.9L134.8,392.9z M179,391.2c-2.9,0.7-4.9,2.6-4.6,4.9c0.3,2,2.9,3.3,5.9,2.6
 	c2.9-0.7,4.9-2.6,4.6-4.6C184.6,392.2,181.9,390.9,179,391.2z M244.8,8C106.1,8,0,113.3,0,252c0,110.9,69.8,205.8,169.5,239.2
@@ -71,16 +51,17 @@
 	C88.7,343.5,87.1,343.8,86.4,344.8z M118.8,380.4c-1.6,1.3-1,4.3,1.3,6.2c2.3,2.3,5.2,2.6,6.5,1c1.3-1.3,0.7-4.3-1.3-6.2
 	C123.1,379.1,120.1,378.8,118.8,380.4z M107.4,365.7c-1.6,1-1.6,3.6,0,5.9s4.3,3.3,5.6,2.3c1.6-1.3,1.6-3.9,0-6.2
 	C111.6,365.4,109,364.4,107.4,365.7L107.4,365.7z"
-            />
-          </svg>
-        </a>
-        <a target="_blank" href="https://codepen.io/jaimegg64">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-            <path
-              d="M502.285 159.704l-234-156c-7.987-4.915-16.511-4.96-24.571 0l-234 156C3.714 163.703 0 170.847 0 177.989v155.999c0 7.143 3.714 14.286 9.715 18.286l234 156.022c7.987 4.915 16.511 4.96 24.571 0l234-156.022c6-3.999 9.715-11.143 9.715-18.286V177.989c-.001-7.142-3.715-14.286-9.716-18.285zM278 63.131l172.286 114.858-76.857 51.429L278 165.703V63.131zm-44 0v102.572l-95.429 63.715-76.857-51.429L234 63.131zM44 219.132l55.143 36.857L44 292.846v-73.714zm190 229.715L61.714 333.989l76.857-51.429L234 346.275v102.572zm22-140.858l-77.715-52 77.715-52 77.715 52-77.715 52zm22 140.858V346.275l95.429-63.715 76.857 51.429L278 448.847zm190-156.001l-55.143-36.857L468 219.132v73.714z"
-            />
-          </svg>
-        </a>
+              />
+            </svg>
+          </a>
+          <a target="_blank" title="CodePen" href="https://codepen.io/jaimegg64">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path
+                d="M502.285 159.704l-234-156c-7.987-4.915-16.511-4.96-24.571 0l-234 156C3.714 163.703 0 170.847 0 177.989v155.999c0 7.143 3.714 14.286 9.715 18.286l234 156.022c7.987 4.915 16.511 4.96 24.571 0l234-156.022c6-3.999 9.715-11.143 9.715-18.286V177.989c-.001-7.142-3.715-14.286-9.716-18.285zM278 63.131l172.286 114.858-76.857 51.429L278 165.703V63.131zm-44 0v102.572l-95.429 63.715-76.857-51.429L234 63.131zM44 219.132l55.143 36.857L44 292.846v-73.714zm190 229.715L61.714 333.989l76.857-51.429L234 346.275v102.572zm22-140.858l-77.715-52 77.715-52 77.715 52-77.715 52zm22 140.858V346.275l95.429-63.715 76.857 51.429L278 448.847zm190-156.001l-55.143-36.857L468 219.132v73.714z"
+              />
+            </svg>
+          </a>
+        </div>
       </div>
       © Copyright {{ year }}. Jaime Garcia All Rights Reserved
     </div>
@@ -139,17 +120,31 @@ export default class Footer extends Vue {
   background-color: $primary;
   margin-top: 1.5rem;
   text-align: center;
+  padding-top: 1rem;
+  padding-bottom: 0.75rem;
   * {
     color: $content-background;
   }
 }
 
 .social {
-  a {
-    svg {
-      width: 2.75rem;
-      padding-right: 1rem;
-      fill: #fffdf5;
+  .social-wrapper {
+    display: flex;
+    flex-direction: row;
+    gap: 1.25rem;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 1rem;
+    margin-top: 0.5rem;
+    a {
+      svg {
+        width: 2rem;
+        fill: #fffdf5;
+        transition: transform 0.2s ease-in-out;
+        &:hover {
+          transform: translateY(-0.35rem);
+        }
+      }
     }
   }
 }
